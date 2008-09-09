5d6cc920116a84a87623f4d6c39db88576fd55df
Julian Raschke <julian@raschke.de>
2008-09-09 20:53:22 +0000

    Removed Numeric extension from ChipmunkIntegration.rb

--- trunk/examples/ChipmunkIntegration.rb
+++ trunk/examples/ChipmunkIntegration.rb
@@ -20,10 +20,7 @@ SCREEN_HEIGHT = 480
 SUBSTEPS = 6
 
 # Convenience method for converting from radians to a Vec2 vector.
-class Numeric   
-  def gosu_to_radians; (self - 90) * Math::PI / 180.0; end
-  def radians_to_gosu; self * 180.0 / Math::PI + 90; end
-
+class Numeric
   def radians_to_vec2
     CP::Vec2.new(Math::cos(self), Math::sin(self))
   end
