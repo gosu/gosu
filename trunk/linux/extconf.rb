2bb3feec6ac35b92f37561aa666f50d1298e2883
Julian Raschke <julian@raschke.de>
2008-09-18 14:31:48 +0000

    Ruby 1.8/1.9 confusion bug fixed

--- trunk/linux/extconf.rb
+++ trunk/linux/extconf.rb
@@ -6,6 +6,8 @@ require 'mkmf'
 
 Dir.chdir File.dirname($0)
 
-exit 1 if not system('env GOSU_MAKEFILE_FOR_GEM=1 sh configure')
+ruby_command = "ruby#{RUBY_VERSION[0..2]}"
+
+exit 1 if not system("env GOSU_MAKEFILE_FOR_GEM=1 GOSU_RUBY_COMMAND=#{ruby_command} sh configure")
 
 exit 0
