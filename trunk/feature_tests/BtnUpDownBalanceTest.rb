b05bf95418fbcbb7d1fdfbcf0a0956023fd443d6
Julian Raschke <julian@raschke.de>
2008-11-15 15:35:53 +0000

    Test to check for key repeat

---
+++ trunk/feature_tests/BtnUpDownBalanceTest.rb
@@ -0,0 +1,22 @@
+require '../lib/gosu'
+
+class Test < Gosu::Window
+  def initialize
+    super(300, 200, false, 20)
+    @downs = @ups = 0
+  end
+  
+  def button_down id
+    @downs += 1
+  end
+  
+  def button_up id
+    @ups += 1
+  end
+  
+  def update
+    self.caption = "#{@downs} downs, #{@ups} ups"
+  end
+end
+
+Test.new.show
