3a690fc2a7c80e3647aee4a47ed0a6828757b9c0
Julian Raschke <julian@raschke.de>
2009-05-04 15:20:02 +0000

    Properly added all UTF/ASCII encodings

--- trunk/GosuImpl/RubyGosuStub.mm
+++ trunk/GosuImpl/RubyGosuStub.mm
@@ -2,8 +2,19 @@
 #include <Gosu/Utility.hpp>
 #include <unistd.h>
 #include "ruby.h"
+#include "oniguruma.h"
 
 extern "C" void Init_gosu();
+
+extern "C" int rb_enc_register(const char *, OnigEncodingType*);
+extern "C" void rb_encdb_declare(const char *name);
+extern OnigEncodingType OnigEncodingASCII;
+extern OnigEncodingType OnigEncodingUS_ASCII;
+extern OnigEncodingType OnigEncodingUTF_8;
+extern OnigEncodingType OnigEncodingUTF_16BE;
+extern OnigEncodingType OnigEncodingUTF_16LE;
+extern OnigEncodingType OnigEncodingUTF_32BE;
+extern OnigEncodingType OnigEncodingUTF_32LE;
 
 int main()
 {
@@ -19,9 +30,20 @@ int main()
     ruby_sysinit(&argc, &argvPointer);
     { 
         RUBY_INIT_STACK; 
+
         ruby_init();
+
+        rb_encdb_declare(OnigEncodingASCII.name);
+        rb_encdb_declare(OnigEncodingUS_ASCII.name);
+        rb_encdb_declare(OnigEncodingUTF_8.name);
+        rb_enc_register(OnigEncodingUTF_16BE.name, &OnigEncodingUTF_16BE);
+        rb_enc_register(OnigEncodingUTF_16LE.name, &OnigEncodingUTF_16LE);
+        rb_enc_register(OnigEncodingUTF_32BE.name, &OnigEncodingUTF_32BE);
+        rb_enc_register(OnigEncodingUTF_32LE.name, &OnigEncodingUTF_32LE);
+
         Init_gosu();
         rb_eval_string("$LOADED_FEATURES << 'gosu.bundle'");
+
         return ruby_run_node(ruby_options(argc, argv)); 
     }
 }
