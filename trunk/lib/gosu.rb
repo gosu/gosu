156f4c139c60e6f41939fdc005e41a16c502d15b
Julian Raschke <julian@raschke.de>
2009-05-06 21:50:00 +0000

    Rakefile adjusted for MacPorts Ruby1.9 compatible gem

--- trunk/lib/gosu.rb
+++ trunk/lib/gosu.rb
@@ -1,7 +1,7 @@
 require 'rbconfig'
 
 if RUBY_VERSION and RUBY_VERSION[0..2] == '1.9' then
-	require "#{File.dirname(__FILE__)}/gosu.for_1_9#{Config::CONFIG['DLEXT']}"
+	require "#{File.dirname(__FILE__)}/gosu.for_1_9.#{Config::CONFIG['DLEXT']}"
 else
-	require "#{File.dirname(__FILE__)}/gosu.for_1_8#{Config::CONFIG['DLEXT']}"
+	require "#{File.dirname(__FILE__)}/gosu.for_1_8.#{Config::CONFIG['DLEXT']}"
 end
