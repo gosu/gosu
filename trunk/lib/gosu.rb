ae740b5af0b89c2c167b693f847fcd8621fa239a
Julian Raschke <julian@raschke.de>
2009-06-28 23:34:47 +0000

    Made gosu.rb more tolerant

--- trunk/lib/gosu.rb
+++ trunk/lib/gosu.rb
@@ -1,7 +1,11 @@
 require 'rbconfig'
 
-if RUBY_VERSION and RUBY_VERSION[0..2] == '1.9' then
-	require "#{File.dirname(__FILE__)}/gosu.for_1_9.#{Config::CONFIG['DLEXT']}"
-else
-	require "#{File.dirname(__FILE__)}/gosu.for_1_8.#{Config::CONFIG['DLEXT']}"
+begin
+  if RUBY_VERSION and RUBY_VERSION[0..2] == '1.9' then
+    require "#{File.dirname(__FILE__)}/gosu.for_1_9.#{Config::CONFIG['DLEXT']}"
+  else
+    require "#{File.dirname(__FILE__)}/gosu.for_1_8.#{Config::CONFIG['DLEXT']}"
+  end
+rescue LoadError => e
+  require "#{File.dirname(__FILE__)}/gosu.#{Config::CONFIG['DLEXT']}"
 end
