19e6d14086fa8740766d3846cd20fd420beb2302
Julian Raschke <julian@raschke.de>
2009-12-18 16:32:48 +0000

    Added missing new "lib/gosu/" folder. That's what I get for not using a GUI once :)

---
+++ trunk/lib/gosu/patches.rb
@@ -0,0 +1,35 @@
+# Extend Numeric with simple angle conversion methods.
+class ::Numeric
+  def gosu_to_radian_scale
+    self * Math::PI / 180.0
+  end
+  def radian_scale_to_gosu
+    self * 180.0 / Math::PI
+  end
+  def gosu_to_radians
+    (self - 90) * Math::PI / 180.0
+  end
+  def radians_to_gosu
+    self * 180.0 / Math::PI + 90
+  end
+end
+    
+# Backwards compatibility: import the constants into Gosu::Button.
+module Gosu::Button
+  Gosu.constants.each { |c| const_set(c, Gosu.const_get(c)) }
+end
+
+# Color constants (SWIG messes up constants somehow)
+class Gosu::Color
+  NONE    = Gosu::Color.new(0x00000000)
+  BLACK   = Gosu::Color.new(0xff000000)
+  GRAY    = Gosu::Color.new(0xff808080)
+  WHITE   = Gosu::Color.new(0xffffffff)            
+  AQUA    = Gosu::Color.new(0xff00ffff)
+  RED     = Gosu::Color.new(0xffff0000)
+  GREEN   = Gosu::Color.new(0xff00ff00)
+  BLUE    = Gosu::Color.new(0xff0000ff)
+  YELLOW  = Gosu::Color.new(0xffffff00)
+  FUCHSIA = Gosu::Color.new(0xffff00ff)
+  CYAN    = Gosu::Color.new(0xff00ffff)
+end
