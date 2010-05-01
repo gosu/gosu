393210b4be987cdcfa506bd3d780c674fc73704e
Julian Raschke <julian@raschke.de>
2010-05-01 16:53:51 +0000

    Some more tweaking, now getting ready to lipo'fy all the built .bundles

--- trunk/mac/Ruby/ppc.sh
+++ trunk/mac/Ruby/ppc.sh
@@ -4,7 +4,7 @@ mkdir -p mac/Ruby/ppc
 cd mac/Ruby/ppc
 export LDFLAGS="-arch ppc"
 export CFLAGS="-O3 -g -isysroot /Developer/SDKs/MacOSX10.4u.sdk -arch ppc -mmacosx-version-min=10.4 -I/Developer/SDKs/MacOSX10.4u.sdk/usr/lib/gcc/powerpc-apple-darwin10/4.0.1/include"
-../source/configure --enable-shared --build=powerpc-apple-darwin8.0
+../source/configure --enable-shared --build=powerpc-apple-darwin8.0 #--prefix=`pwd`/jail <- gem inside there does not work
 ruby -e 'ls = File.readlines("Makefile"); ls.each { |l| l.gsub!(/ARCH_FLAG =/, "ARCH_FLAG = -arch ppc") }; File.open("Makefile", "w") { |f| ls.each { |l| f.puts l } }'
 make
 install_name_tool -id @executable_path/../Frameworks/libruby.1.9.1.dylib libruby.1.9.1.dylib
--- trunk/rake/ruby19.rb
+++ trunk/rake/ruby19.rb
@@ -3,19 +3,24 @@ TARGET_ROOT   = "mac/Ruby"
 SOURCE_ROOT   = "#{TARGET_ROOT}/source"
 ALL_PLATFORMS = [:ppc, :i386, :x86_64]
 
+def merge_lib source_file, target_file
+  if File.exist? target_file then
+    sh "lipo #{source_file} #{target_file} -create #{target_file}"
+  else
+    sh "cp #{source_file} #{target_file}"
+  end
+end
+
 namespace :ruby19 do
   ALL_PLATFORMS.each do |platform|
     task platform.to_sym do
+      # Compile Ruby and libruby
       sh "bash #{TARGET_ROOT}/#{platform}.sh"
-
+      
+      # Merge libruby with existing platforms
       source_file = "#{TARGET_ROOT}/#{platform}/#{RUBY_DYLIB}"
       target_file = "#{TARGET_ROOT}/lib/#{RUBY_DYLIB}"
-      
-      if File.exist? target_file then
-        sh "lipo #{source_file} #{target_file} -create #{target_file}"
-      else
-        sh "cp #{source_file} #{target_file}"
-      end
+      merge_lib source_file, target_file
     end
   end
   
@@ -25,5 +30,11 @@ namespace :ruby19 do
   
   task :build => [:copy_headers] + ALL_PLATFORMS do
     # yessir
+  end
+  
+  task :cleanup do
+    ALL_PLATFORMS.each do |platform|
+      sh "rm -rf #{TARGET_ROOT}/#{platform}"
+    end
   end
 end
