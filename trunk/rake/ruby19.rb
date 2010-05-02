9649885af81144a58c0ae3ddcd4d812f9a19f9f1
Julian Raschke <julian@raschke.de>
2010-05-02 07:30:38 +0000

    Added complete Ruby 1.9 stdlib (with KILLLIST excluding rubygems, rdoc etc. etc.)

--- trunk/rake/ruby19.rb
+++ trunk/rake/ruby19.rb
@@ -1,7 +1,8 @@
-RUBY_DYLIB    = "libruby.1.9.1.dylib"
-TARGET_ROOT   = "mac/Ruby"
-SOURCE_ROOT   = "#{TARGET_ROOT}/source"
-ALL_PLATFORMS = [:ppc, :i386, :x86_64]
+RUBY_DYLIB      = "libruby.1.9.1.dylib"
+TARGET_ROOT     = "mac/Ruby"
+SOURCE_ROOT     = "#{TARGET_ROOT}/source"
+ALL_PLATFORMS   = [:ppc, :i386, :x86_64]
+STDLIB_KILLLIST = %w(README irb rake* racc rdoc* *ubygems* cgi* readline* tkutil* tcltklib* rss*)
 
 def merge_lib source_file, target_file
   if File.exist? target_file then
@@ -23,9 +24,10 @@ namespace :ruby19 do
       
       # Merge libruby with existing platforms
       source_file = "#{TARGET_ROOT}/#{platform}/#{RUBY_DYLIB}"
-      target_file = "#{TARGET_ROOT}/lib/#{RUBY_DYLIB}"
+      target_file = "#{TARGET_ROOT}/#{RUBY_DYLIB}"
       merge_lib source_file, target_file
       
+      # Copy binary libraries
       Dir["#{TARGET_ROOT}/#{platform}/.ext/*darwin*/*.bundle"].each do |source_file|
         target_file = "#{TARGET_ROOT}/lib/#{File.basename(source_file)}"
         merge_lib source_file, target_file
@@ -35,6 +37,14 @@ namespace :ruby19 do
   
   task :copy_headers do
     sh "cp -R #{SOURCE_ROOT}/include #{TARGET_ROOT}"
+  end
+  
+  task :copy_stdlib do
+    sh "mkdir -p #{TARGET_ROOT}/lib"
+    sh "cp -R #{SOURCE_ROOT}/lib/* #{TARGET_ROOT}/lib/"
+    STDLIB_KILLLIST.each do |item|
+      sh "rm -rf #{TARGET_ROOT}/lib/#{item}"
+    end
   end
   
   task :build => [:cleanup, :copy_headers] + ALL_PLATFORMS do
