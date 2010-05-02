c1613ed85334283e79778052b121d1feff719916
Julian Raschke <julian@raschke.de>
2010-05-02 06:12:13 +0000

    Fixed i386 build script Added /lib/ directory to RubyGosu App.app (for stdlib) Fixed some Rakefile things

--- trunk/lib/gosu/run.rb
+++ trunk/lib/gosu/run.rb
@@ -1,7 +1,9 @@
-# Used by the RubyGosu App.app deployment package.
-
+# Replace the load path
 $LOAD_PATH.clear
 $LOAD_PATH << File.dirname(__FILE__)[0..-6]
+$LOAD_PATH << $LOAD_PATH[0] + '/lib'
+# Ruby portions of Gosu
 require 'gosu/patches'
 require 'gosu/swig_patches'
+# Main application
 require 'Main'
--- trunk/rake/ruby19.rb
+++ trunk/rake/ruby19.rb
@@ -5,7 +5,7 @@ ALL_PLATFORMS = [:ppc, :i386, :x86_64]
 
 def merge_lib source_file, target_file
   if File.exist? target_file then
-    sh "lipo #{source_file} #{target_file} -create #{target_file}"
+    sh "lipo #{source_file} #{target_file} -create -output #{target_file}"
   else
     sh "cp #{source_file} #{target_file}"
   end
@@ -17,10 +17,19 @@ namespace :ruby19 do
       # Compile Ruby and libruby
       sh "bash #{TARGET_ROOT}/#{platform}.sh"
       
+      # Copy platform specific config.h
+      sh "mkdir -p #{TARGET_ROOT}/include/#{platform}/ruby"
+      sh "cp #{TARGET_ROOT}/#{platform}/.ext/include/*/ruby/*.h #{TARGET_ROOT}/include/#{platform}/ruby/"
+      
       # Merge libruby with existing platforms
       source_file = "#{TARGET_ROOT}/#{platform}/#{RUBY_DYLIB}"
       target_file = "#{TARGET_ROOT}/lib/#{RUBY_DYLIB}"
       merge_lib source_file, target_file
+      
+      Dir["#{TARGET_ROOT}/#{platform}/.ext/*darwin*/*.bundle"].each do |source_file|
+        target_file = "#{TARGET_ROOT}/lib/#{File.basename(source_file)}"
+        merge_lib source_file, target_file
+      end
     end
   end
   
@@ -28,11 +37,13 @@ namespace :ruby19 do
     sh "cp -R #{SOURCE_ROOT}/include #{TARGET_ROOT}"
   end
   
-  task :build => [:copy_headers] + ALL_PLATFORMS do
+  task :build => [:cleanup, :copy_headers] + ALL_PLATFORMS do
     # yessir
   end
   
   task :cleanup do
+    sh "rm -rf #{TARGET_ROOT}/lib/*"
+    sh "rm -rf #{TARGET_ROOT}/include/*"
     ALL_PLATFORMS.each do |platform|
       sh "rm -rf #{TARGET_ROOT}/#{platform}"
     end
