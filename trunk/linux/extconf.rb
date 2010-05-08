07cea3a8ed1ed5f9d4ab0c689f0bf5d04d20f2de
Julian Raschke <julian@raschke.de>
2010-05-08 16:22:59 +0000

    Added Gosu's own headers to mkmf

--- trunk/linux/extconf.rb
+++ trunk/linux/extconf.rb
@@ -28,7 +28,7 @@ require 'mkmf'
 # Copy all relevant C++ files into the current directory (rofl)
 
 SOURCE_FILES.each { |file| `cp ../GosuImpl/#{file} #{File.basename(file)}` }
-$INCFLAGS << ' -I../Gosu/'
+$INCFLAGS << ' -I../'
 
 create_makefile("gosu")
 
