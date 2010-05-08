8eda3b29a66dd9b28a25c20441be31c323217086
Julian Raschke <julian@raschke.de>
2010-05-08 15:41:47 +0000

    First feeble attempt to use mkmf for Ruby/Gosu, continuing dev on OS X, hence committing

--- trunk/linux/extconf.rb
+++ trunk/linux/extconf.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/env ruby
-# Bogus extconf.rb to bridge rubygems to autoconf.
-# rubygems will call "make install" on the resulting makefile.
 
 if defined? RUBY_PLATFORM and (RUBY_PLATFORM['-win32'] or RUBY_PLATFORM['win32-'] or RUBY_PLATFORM['-mingw32']) then
-  puts 'This gem is not meant to be installed on Windows.'
+  puts 'This gem is not meant to be installed on Windows. If you see this message, gem made a mistake.'
   puts 'On Windows, please use:'
   puts 'gem install gosu --platform=mswin32'
   exit 1
@@ -14,10 +12,9 @@ puts 'See the following site for a list:'
 puts 'http://code.google.com/p/gosu/wiki/GettingStartedOnLinux'
 puts
 
-require 'rbconfig'
+require 'mkmf'
 
-Dir.chdir File.dirname($0)
+dir_config("gosu")
 
-ruby_command = File.join(Config::CONFIG["bindir"], Config::CONFIG["RUBY_INSTALL_NAME"])
+create_makefile("gosu")
 
-exit 1 if not system("env GOSU_MAKEFILE_FOR_GEM=1 GOSU_RUBY_COMMAND=#{ruby_command} sh configure")
