26ee8deea3a8f82b2ea0ccb4aa0944cd94b69607
Julian Raschke <julian@raschke.de>
2010-11-10 15:48:52 +0000

    Made Unicode script more variable

--- trunk/feature_tests/unicode_text_width.rb
+++ trunk/feature_tests/unicode_text_width.rb
@@ -3,8 +3,12 @@ require '../lib/gosu'
 class Test < Gosu::Window
   def initialize
     super(800, 600, false)
-	font = Gosu::Font.new(self, "Arial", 20)
-	self.caption = "U=#{font.text_width 'U'} Ü=#{font.text_width 'Ü'}"
+	@font = Gosu::Font.new(self, "Arial", 20)
+	self.text_input = Gosu::TextInput.new
+  end
+  
+  def update
+	self.caption = "#{text_input.text}=#{@font.text_width text_input.text}"
   end
 end
 
