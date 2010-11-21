99da2764b819cbb00d867ffe126e2f1b06e4ddd4
Julian Raschke <julian@raschke.de>
2010-11-21 20:55:27 +0000

    Fixed "am I jlnr on OS X" detection

--- trunk/rake/todo.rb
+++ trunk/rake/todo.rb
@@ -1,4 +1,4 @@
-if ENV['USER'] == 'jlnr' and `uname` == 'Darwin' then
+if ENV['USER'] == 'jlnr' and `uname`.chomp == 'Darwin' then
   require 'appscript'
 
   desc "Publish OmniFocus tasks in ToDo Wiki page"
