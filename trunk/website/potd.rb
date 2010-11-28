fae34e113748096381bea6e152044700ace987eb
Julian Raschke <julian@raschke.de>
2010-11-28 02:13:51 +0000

    Fixed potd script again

--- trunk/website/potd.rb
+++ trunk/website/potd.rb
@@ -3,32 +3,35 @@ require 'mysql'
 require 'RMagick'
 
 begin
-  # connect to the MySQL server
-  dbh = Mysql.real_connect("localhost", ARGV[0], ARGV[1], "mwf")
-  res = dbh.query %( select t.id, t.subject, a.postId, a.fileName
-                     from topics t join attachments a on (t.basePostId = a.postId)
-                     where boardId = (select id from boards where title = 'Gosu Showcase' and
-                                       (a.filename like '%.png' or a.filename like '%.jpg')) )
-  ary = []
-  res.each { |row| ary << row }
-  res.free
+  begin
+    # connect to the MySQL server
+    dbh = Mysql.real_connect("localhost", ARGV[0], ARGV[1], "mwf")
+    res = dbh.query %( select t.id, t.subject, a.postId, a.fileName
+                       from topics t join attachments a on (t.basePostId = a.postId)
+                       where boardId = (select id from boards where title = 'Gosu Showcase' and
+                                         (a.filename like '%.png' or a.filename like '%.jpg')) )
+    ary = []
+    res.each { |row| ary << row }
+    res.free
   
-  winner = ary[rand(ary.size)]
+    winner = ary[rand(ary.size)]
   
-  filename = "http://www.libgosu.org/mwf/attachments/#{winner[2].to_i % 100}/#{winner[2]}/#{winner[3]}"
-  retry if not File.exist? filename
+    filename = "http://www.libgosu.org/mwf/attachments/#{winner[2].to_i % 100}/#{winner[2]}/#{winner[3]}"
+    retry if not File.exist? filename
   
-  File.open('potd_values.rb', 'w') do |file|
-    file.puts "$POTD_ID = #{winner[0]}"
-    file.puts "$POTD_TITLE = %(#{winner[1]})"
+    File.open('potd_values.rb', 'w') do |file|
+      file.puts "$POTD_ID = #{winner[0]}"
+      file.puts "$POTD_TITLE = %(#{winner[1]})"
+    end
+  
+    image = Magick::Image.read(filename)[0]
+    image.change_geometry! '276x350>' do |cols, rows, img|
+      img.resize! cols, rows
+    end
+    image.write 'potd.png'
+  rescue
+    # only for retry :P
   end
-  
-  image = Magick::Image.read(filename)[0]
-  image.change_geometry! '276x350>' do |cols, rows, img|
-    img.resize! cols, rows
-  end
-  image.write 'potd.png'
-  
 ensure
   # disconnect from server
   dbh.close if dbh
