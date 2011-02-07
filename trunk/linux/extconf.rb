9f42f21f4509ee76332e4288dcc32497ea04c59e
Julian Raschke <julian@raschke.de>
2011-02-07 07:34:52 +0000

    Reverted to SDL_mixer (I think)

--- trunk/linux/extconf.rb
+++ trunk/linux/extconf.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
 
-if defined? RUBY_PLATFORM and (RUBY_PLATFORM['-win32'] or RUBY_PLATFORM['win32-'] or RUBY_PLATFORM['mswin'] or RUBY_PLATFORM['mingw32']) then
-  puts 'This gem is not meant to be installed on Windows. If you see this message, gem made a mistake.'
-  puts 'On Windows, please use:'
+WINDOWS_HINTS = %w(-win32 win32- mswin mingw32)
+
+if defined? RUBY_PLATFORM and WINDOWS_HINTS.any? { |hint| RUBY_PLATFORM.contain? hint } then
+  puts 'This gem is not meant to be installed on Windows. Instead, please use:'
   puts 'gem install gosu --platform=i386-mingw32'
   exit 1
 end
@@ -13,36 +14,54 @@ puts 'http://code.google.com/p/gosu/wiki/GettingStartedOnLinux'
 puts
 
 # FIXME should reversely filter out files ending in Win, Mac, Touch, AL etc.
-SOURCE_FILES =
-    %w(Math.cpp Utility.cpp IO.cpp FileUnix.cpp InputX.cpp TextInputX.cpp TimingUnix.cpp WindowX.cpp
-	     Graphics/Bitmap.cpp Graphics/BitmapUtils.cpp Graphics/Color.cpp
-	     Graphics/TexChunk.cpp Graphics/Graphics.cpp Graphics/Image.cpp
-	     Graphics/BlockAllocator.cpp Inspection.cpp
-	     Graphics/Texture.cpp Graphics/LargeImageData.cpp
-	     Graphics/BitmapPNG.cpp Graphics/Font.cpp Graphics/BitmapBMP.cpp
-	     Graphics/TextUnix.cpp Graphics/Text.cpp Graphics/Transform.cpp
-	     Graphics/BitmapColorKey.cpp DirectoriesUnix.cpp
-	     Audio/AudioAudiere.cpp RubyGosu_wrap.cxx)
+SOURCE_FILES = %w(
+  Audio/AudioSDL.cpp
+  DirectoriesUnix.cpp
+  FileUnix.cpp
+  Graphics/Bitmap.cpp
+  Graphics/BitmapBMP.cpp
+  Graphics/BitmapColorKey.cpp
+  Graphics/BitmapPNG.cpp
+  Graphics/BitmapUtils.cpp
+  Graphics/BlockAllocator.cpp
+  Graphics/Color.cpp
+  Graphics/Font.cpp
+  Graphics/Graphics.cpp
+  Graphics/Image.cpp
+  Graphics/LargeImageData.cpp
+  Graphics/TexChunk.cpp
+  Graphics/Text.cpp
+  Graphics/TextUnix.cpp
+  Graphics/Texture.cpp
+  Graphics/Transform.cpp
+  InputX.cpp
+  Inspection.cpp
+  IO.cpp
+  Math.cpp
+  RubyGosu_wrap.cxx
+  TextInputX.cpp
+  TimingUnix.cpp
+  Utility.cpp
+  WindowX.cpp
+)
 
 require 'mkmf'
 
 # Copy all relevant C++ files into the current directory
-# FIXME could be done by gem packager
+# FIXME Could be done by gem task instead.
 SOURCE_FILES.each { |file| `cp ../GosuImpl/#{file} #{File.basename(file)}` }
 
 # Symlink our pretty gosu.so into ../lib
-# FIXME gosu.rb should just look in the right place
+# FIXME gosu.rb should just look in the right place.
 `ln -s ../linux/gosu.so ../lib/gosu.so`
 
 sdl_config = with_config("sdl-config", "sdl-config")
-audiere_config = with_config("audiere-config", "audiere-config")
-pango_config = "pkg-config pangoft2" # FIXME should probably use with_config
+pango_config = "pkg-config pangoft2"
 
-$INCFLAGS << " -I../ -I../GosuImpl `#{sdl_config} --cflags` `#{audiere_config} --cxxflags` `#{pango_config} --cflags`"
-$LDFLAGS << " `#{sdl_config} --libs` `#{audiere_config} --libs` `#{pango_config} --libs` -lX11"
+$INCFLAGS << " -I../ -I../GosuImpl `#{sdl_config} --cflags` `#{pango_config} --cflags`"
+$LDFLAGS << " `#{sdl_config} --libs` `#{pango_config} --libs` -lX11"
 have_header('SDL_ttf.h') if have_library('SDL_ttf', 'TTF_RenderUTF8_Blended')
 have_header('gl.h') if have_library('GL', 'glMatrixMode')
 have_header('png.h') if have_library('png', 'png_sig_cmp')
 
 create_makefile("gosu")
-
