fae6c832f793a4e1bed1a3fdff461d86a2b07532
Julian Raschke <julian@raschke.de>
2011-06-04 06:55:25 +0200

    Added optional support for FreeImage.dll in Ruby version

---
+++ feature_tests/exotic_image.rb
@@ -0,0 +1,14 @@
+require '../lib/gosu'
+
+class Test < Gosu::Window
+  def initialize 
+    super(640, 480, false)
+    @image = Gosu::Image.new(self, "media/Test.psd", false)
+  end
+  
+  def draw
+    @image.draw 0, 0, 0
+  end
+end
+
+Test.new.show
