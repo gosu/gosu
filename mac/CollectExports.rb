6b106ef1c994a64b68b1a6f57cc32c8cf8ee1d7d
Julian Raschke <julian@raschke.de>
2011-06-24 16:40:31 +0800

    Removed obsolete CollectExports.rb
    Signed-off-by: Julian Raschke <julian@raschke.de>

--- mac/CollectExports.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-output = `nm '../RubyGosu\ App.app/Contents/MacOS/RubyGosu\ App'`
-exports = output.grep /\ (_rb_|_ruby_|_st_|_SIGNED)/
-exports.map! { |line| line.split(' ').last }
-exports.reject! { |symbol| symbol.include? '.' }
-puts exports
