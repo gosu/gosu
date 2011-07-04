7dd83a16b0f62494f33324b21a5f29ca51c1f223
Julian Raschke <julian@raschke.de>
2011-07-04 12:45:34 +0200

    Exorcism commit of Swiss tabs

--- GosuImpl/Graphics/Graphics.cpp
+++ GosuImpl/Graphics/Graphics.cpp
@@ -327,10 +327,11 @@ void Gosu::Graphics::popTransform()
 {
     pimpl->currentTransforms.pop_back();
     Transform result = scale(1);
-
-		for (Transforms::reverse_iterator it = pimpl->currentTransforms.rbegin(); it != pimpl->currentTransforms.rend(); ++it)
-			result = multiply(result, *it);
-
+    
+    for (Transforms::reverse_iterator it = pimpl->currentTransforms.rbegin(),
+         end = pimpl->currentTransforms.rend(); it != end; ++it)
+        result = multiply(result, *it);
+    
     ensureBackOfList(pimpl->absoluteTransforms, result);
 }
 
