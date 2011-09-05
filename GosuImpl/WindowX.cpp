60226e8e2a917bc7ac3fcd0fe50d47fc7663a675
Paul Merrill <napalminc@gmail.com>
2011-09-05 15:15:07 +0800

    Revert "X11: redraw the window on window damage"
    This reverts commit fc54821d9086f48ccbe2a5abc1786edf42377c23.

--- GosuImpl/WindowX.cpp
+++ GosuImpl/WindowX.cpp
@@ -20,7 +20,6 @@
 #include <GL/glx.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
-#include <X11/extensions/Xdamage.h>
 #include "X11vroot.h"
 
 using namespace std::tr1::placeholders;
@@ -99,10 +98,6 @@ struct Gosu::Window::Impl
     ::GLXContext context;
     ::Window window;
     ::XVisualInfo* visual;
-
-    // We use the XDamage extension to detect when we need to redraw ourselves
-    Damage damage;
-    int damageEvent, damageError;
     
     // Last set title
     std::wstring title;
@@ -165,22 +160,6 @@ struct Gosu::Window::Impl
                 else if (event.type == FocusOut)
                     active = false;
             }
-
-            // Force redraw on window damage; don't check needsRedraw()
-            if (event.type == damageEvent + XDamageNotify)
-            {
-                XDamageNotifyEvent* dev = (XDamageNotifyEvent*)&event;
-                XDamageSubtract(display, dev->damage, None, None);
-                // If there are more events queued up right after this one, we
-                // don't need to redraw just yet
-                if (dev->more == false &&
-                        window->graphics().begin(Colors::black))
-                {
-                    window->draw();
-                    window->graphics().end();
-                    glXSwapBuffers(display, this->window);
-                }
-            }
         }
         
         if (showingCursor && !window->needsCursor())
@@ -205,18 +184,6 @@ struct Gosu::Window::Impl
             window->graphics().end();
             glXSwapBuffers(display, this->window);
         }
-    }
-
-    // The ICCCM standard requires that compositing window managers acquire
-    // ownership of a selection named _NET_WM_CM_Sn, where n is the screen
-    // number
-    bool isComposited()
-    {
-        std::ostringstream atomName;
-        atomName << "_NET_WM_CM_S" << XDefaultScreen(display);
-        const std::string& tmp = atomName.str();
-        Atom selection = XInternAtom(display, tmp.c_str(), False);
-        return XGetSelectionOwner(display, selection) != None;
     }
 };
 
@@ -315,21 +282,10 @@ Gosu::Window::Window(unsigned width, unsigned height, bool fullscreen,
         input().setMouseFactors(1.0 * width / pimpl->width,
                   1.0 * height / pimpl->height);
     }
-
-    // Composited windows, by nature, don't need to monitor XDamage
-    if (!pimpl->isComposited())
-    {
-        // Register our intent to monitor this window for damage
-        XDamageQueryExtension(pimpl->display, &pimpl->damageEvent, &pimpl->damageError);
-        pimpl->damage = XDamageCreate(pimpl->display, pimpl->window,
-                XDamageReportNonEmpty);
-    }
 }
 
 Gosu::Window::~Window()
 {
-    if (!pimpl->isComposited())
-        XDamageDestroy(pimpl->display, pimpl->damage);
     XFreeCursor(pimpl->display, pimpl->emptyCursor);
     XDestroyWindow(pimpl->display, pimpl->window);
     XSync(pimpl->display, false);
