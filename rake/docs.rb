620b49a5cc98bf3a2cd159d46bfc7f8aefec6902
Julian Raschke <julian@raschke.de>
2012-01-22 00:17:39 +0800

    Remove workaround around yard <0.7.4 bug

--- rake/docs.rb
+++ rake/docs.rb
@@ -1,10 +1,7 @@
 # For some reason, YARD::Rake::YardocTask does not work for me at all.
 desc "Build Ruby reference with YARD"
 task :ruby_docs do
-  # Also work around the fact that yardoc does not work at all for me in 1.8
-  # (Blaming a missing rdoc gem)
-  # Oh, and not in 1.9.2 either...
-  sh "rvm 1.9.3 do yardoc || yardoc"
+  sh "yardoc"
 end
 
 desc "Build C++ reference with doxygen"
