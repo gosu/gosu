365068420afb37515d362052d1b5f31d43327f1e
Quit <zaubermuffin@gmail.com>
2012-05-29 21:07:11 +0200

    Gosu::milliseconds returns milliseconds since start on Win/Unix
    There's still this 149-runtime-days-overflow, but at least it won't happen "randomly" anymore.

--- GosuImpl/TimingUnix.cpp
+++ GosuImpl/TimingUnix.cpp
@@ -9,10 +9,16 @@ void Gosu::sleep(unsigned milliseconds)
 
 unsigned long Gosu::milliseconds()
 {
+		static unsigned long start = 0;
+
     timeval tp;
     gettimeofday(&tp, NULL);
+
+		if (!start)
+			start = tp.tv_usec / 1000UL + tp.tv_sec * 1000UL;
+		
     // Truncate to 2^30, C++ users shouldn't mind and Ruby users will
     // have a happy GC on 32-bit systems.
     // No, don't ask why this is an unsigned long then :)
-    return (tp.tv_usec / 1000UL + tp.tv_sec * 1000UL) & 0x1fffffff;
+    return (tp.tv_usec / 1000UL + tp.tv_sec * 1000UL - start) & 0x1fffffff;
 }
