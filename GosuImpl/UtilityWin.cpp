8112ab507ba0193d3369d04bde2c2032fbec3efb
Julian Raschke <julian@raschke.de>
2013-04-30 10:32:58 +1000

    Move Windows-specific code in Utility.cpp to UtilityWin.cpp

--- GosuImpl/UtilityWin.cpp
+++ GosuImpl/UtilityWin.cpp
@@ -3,6 +3,20 @@
 #include <windows.h>
 using namespace std;
 
+wstring Gosu::utf8ToWstring(const string& utf8)
+{
+	vector<wchar_t> buffer(utf8.size() + 1);
+	MultiByteToWideChar(CP_UTF8, 0, utf8.c_str(), utf8.size() + 1, &buffer[0], buffer.size());
+	return &buffer[0];
+}	
+string Gosu::wstringToUTF8(const wstring& ws)
+{
+	unsigned size = WideCharToMultiByte(CP_UTF8, 0, ws.c_str(), ws.size(), 0, 0, 0, 0);
+	vector<char> buffer(size + 1);
+	WideCharToMultiByte(CP_UTF8, 0, ws.c_str(), ws.size(), &buffer[0], buffer.size(), 0, 0);
+	return &buffer[0];
+}
+
 string Gosu::language()
 {
     LCID lcid = GetUserDefaultLCID();
