b3239d19ffc347c1e998af124ebcd1b6e34e35c9
Julian Raschke <julian@raschke.de>
2014-12-12 17:34:00 +0100

    Fix LoadError in gosu.rb on Windows

--- lib/gosu.rb
+++ lib/gosu.rb
@@ -1,20 +1,19 @@
 require 'rbconfig'
 
 if RUBY_PLATFORM =~ /mswin$|mingw32|win32-|-win32/ then
+  binary_path = File.dirname(__FILE__)
   # 64-bit builds of Windows use "x64-mingw32" as RUBY_PLATFORM
-  suffix = '64' if RUBY_PLATFORM =~ /^x64-/
+  binary_path += '64' if RUBY_PLATFORM =~ /^x64-/
   
   # Add this gem to the PATH on Windows so that DLLs can be found.
-  ENV['PATH'] = "#{File.dirname(__FILE__)}#{suffix};#{ENV['PATH']}"
+  ENV['PATH'] = "#{binary_path};#{ENV['PATH']}"
+  
+  # Add the correct directory 
+  RUBY_VERSION =~ /(\d+.\d+)/
+  $LOAD_PATH.unshift "#{File.dirname(__FILE__)}#{suffix}/#{$1}"
 end
 
-begin
-  # If possible, use a precompiled binary.
-  RUBY_VERSION =~ /(\d+.\d+)/
-  require "../lib#{suffix}/#{$1}/gosu.#{RbConfig::CONFIG['DLEXT']}"
-rescue LoadError
-  require "gosu.#{RbConfig::CONFIG['DLEXT']}"
-end
+require "gosu.#{RbConfig::CONFIG['DLEXT']}"
 
 require "gosu/swig_patches"
 require "gosu/patches"
