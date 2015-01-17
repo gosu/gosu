78a0e0e74a55df6bc2eba2d1b1de9ab16b8b03f6
Julian Raschke <julian@raschke.de>
2015-01-17 15:10:12 +0000

    Fix lib/gosu.rb, was embarrassingly broken in previous commit

--- lib/gosu.rb
+++ lib/gosu.rb
@@ -5,12 +5,12 @@ if RUBY_PLATFORM =~ /mswin$|mingw32|win32-|-win32/ then
   # 64-bit builds of Windows use "x64-mingw32" as RUBY_PLATFORM
   binary_path += '64' if RUBY_PLATFORM =~ /^x64-/
   
-  # Add this gem to the PATH on Windows so that DLLs can be found.
+  # Add this gem to the PATH on Windows so that bundled DLLs can be found.
   ENV['PATH'] = "#{binary_path};#{ENV['PATH']}"
   
   # Add the correct directory 
   RUBY_VERSION =~ /(\d+.\d+)/
-  $LOAD_PATH.unshift "#{File.dirname(__FILE__)}#{suffix}/#{$1}"
+  $LOAD_PATH.unshift "#{binary_path}/#{$1}"
 end
 
 require "gosu.#{RbConfig::CONFIG['DLEXT']}"
