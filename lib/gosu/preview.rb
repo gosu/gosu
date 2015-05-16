41c7ddb34bb2426a51dc6d93299f031715b6f360
Julian Raschke <julian@raschke.de>
2015-05-16 11:28:11 +0200

    Replace preview.rb by a long warning

--- lib/gosu/preview.rb
+++ lib/gosu/preview.rb
@@ -1,17 +1,10 @@
+warn "gosu/preview.rb has been removed in Gosu 0.9.0, and Gosu itself \n" +
+  "provides a similar interface to what preview.rb used to offer.\n" +
+  "Notable differences:\n" +
+  "• no global $window variable\n" +
+  "• no global Gosu.mouse_x and Gosu.mouse_y functions\n" +
+  "• Image#initialize et.al. use an options hash now\n" +
+  "If you cannot update your code base right now, you should require \n" +
+  "Gosu 0.8.x in your Gemfile: gem 'gosu', '~> 0.8.0'";
+
 require 'gosu'
-
-# Wrapper around Gosu 0.4-0.8 that provides the work-in-progress 0.9 interface
-
-module Gosu
-  def self.mouse_x
-    $window.mouse_x
-  end
-  
-  def self.mouse_y
-    $window.mouse_y
-  end
-  
-  def self.draw_rect x, y, width, height, color = 0xffffffff, z = 0, mode = :default
-    Gosu::draw_quad x, y, color, x + width, y, color, x, y + height, color, x + width, y + height, color, z, mode
-  end
-end
