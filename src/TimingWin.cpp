3d3b83ce00e72ee1d949878656d300f9599c35e8
Julian Raschke <julian@raschke.de>
2015-05-17 09:50:03 +0000

    Clean up this use of atexit() while we're at it

--- src/TimingWin.cpp
+++ src/TimingWin.cpp
@@ -4,29 +4,11 @@
 
 void Gosu::sleep(unsigned milliseconds)
 {
-    ::Sleep(milliseconds);
-}
-
-namespace
-{
-    void resetTGT()
-    {
-        ::timeEndPeriod(1);
-    }
+    Sleep(milliseconds);
 }
 
 unsigned long Gosu::milliseconds()
 {
-		static unsigned long start = 0;
-
-    if (!start)
-    {
-        if (::timeBeginPeriod(1) != TIMERR_NOERROR)
-            std::atexit(resetTGT);
-				start = ::timeGetTime();
-    }
-    // Truncate to 2^30, C++ users shouldn't mind and Ruby users will
-    // have a happy GC on 32-bit systems.
-    // No, don't ask why this is an unsigned long then :)
-    return (::timeGetTime() - start) & 0x1fffffff;
+    static unsigned long start = (timeBeginPeriod(1), timeGetTime());
+    return timeGetTime() - start;
 }
