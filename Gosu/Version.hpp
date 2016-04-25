369d7c6d418a49053c58854fb15cecf456619e1c
Julian Raschke <julian@raschke.de>
2016-04-25 09:21:03 +0200

    Bump version to 0.10.7

--- Gosu/Version.hpp
+++ Gosu/Version.hpp
@@ -3,13 +3,13 @@
 
 #define GOSU_MAJOR_VERSION 0
 #define GOSU_MINOR_VERSION 10
-#define GOSU_POINT_VERSION 6
-#define GOSU_VERSION "0.10.6"
+#define GOSU_POINT_VERSION 7
+#define GOSU_VERSION "0.10.7"
 
 #define GOSU_COPYRIGHT_NOTICE \
     "This software uses the following third-party libraries:\n" \
     "\n" \
-    "Gosu, http://www.libgosu.org, MIT License, http://opensource.org/licenses/MIT\n" \
+    "Gosu, https://www.libgosu.org, MIT License, http://opensource.org/licenses/MIT\n" \
     "SDL 2, http://www.libsdl.org, MIT License, http://opensource.org/licenses/MIT\n" \
     "libsndfile, http://www.mega-nerd.com/libsndfile, GNU LGPL 3, http://www.gnu.org/copyleft/lesser.html\n" \
     "OpenAL Soft, http://kcat.strangesoft.net/openal.html, GNU LGPL 2, http://www.gnu.org/licenses/old-licenses/lgpl-2.0.html\n"
