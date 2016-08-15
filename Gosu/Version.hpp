530887bce394045909e029a8f4c661f3a6c8577b
Julian Raschke <julian@raschke.de>
2016-08-15 19:52:33 +0200

    Bump version to 0.10.8

--- Gosu/Version.hpp
+++ Gosu/Version.hpp
@@ -3,8 +3,8 @@
 
 #define GOSU_MAJOR_VERSION 0
 #define GOSU_MINOR_VERSION 10
-#define GOSU_POINT_VERSION 7
-#define GOSU_VERSION "0.10.7"
+#define GOSU_POINT_VERSION 8
+#define GOSU_VERSION "0.10.8"
 
 #define GOSU_COPYRIGHT_NOTICE \
     "This software uses the following third-party libraries:\n" \
