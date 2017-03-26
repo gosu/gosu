28ef1a51809be6f399ae0a4412243d75d866c7a1
Julian Raschke <julian@raschke.de>
2017-03-26 11:27:53 +0200

    Tidy up lib/gosu.rb

--- lib/gosu.rb
+++ lib/gosu.rb
@@ -1,19 +1,21 @@
-require 'rbconfig'
+require "rbconfig"
 
 if RUBY_PLATFORM =~ /mswin$|mingw32|mingw64|win32\-|\-win32/
   binary_path = File.dirname(__FILE__)
   # 64-bit builds of Windows use "x64-mingw32" as RUBY_PLATFORM
-  binary_path += '64' if RUBY_PLATFORM =~ /^x64-/
+  binary_path += "64" if RUBY_PLATFORM =~ /^x64-/
   
   # Add this gem to the PATH on Windows so that bundled DLLs can be found.
-  ENV['PATH'] = "#{binary_path.encode ENV['PATH'].encoding};#{ENV['PATH']}"
+  # When running through Ocra on Windows, we need to be careful to preserve the ENV["PATH"]
+  # encoding (see #385).
+  ENV["PATH"] = "#{binary_path.encode ENV["PATH"].encoding};#{ENV["PATH"]}"
   
   # Add the correct directory 
   RUBY_VERSION =~ /(\d+.\d+)/
   $LOAD_PATH.unshift "#{binary_path}/#{$1}"
 end
 
-require "gosu.#{RbConfig::CONFIG['DLEXT']}"
+require "gosu.#{RbConfig::CONFIG["DLEXT"]}"
 
 require "gosu/swig_patches"
 require "gosu/patches"
