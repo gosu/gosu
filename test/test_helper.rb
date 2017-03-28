65eceb26c60ffc55144cb149c004ca9b366e147d
Kjarrigan <github@kjarrigan.de>
2017-03-28 23:09:40 +0200

    Add some actual helpers to test_helper and use it

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,21 @@ gem 'minitest'
 require "minitest/autorun"
 require "gosu" unless defined? Gosu
 
+module TestHelper
+  def media_path(fname='')
+    File.join(__dir__, "media", fname)
+  end
+end
+
 module InteractiveTests
+  def interactive_cli(message)
+    return false unless ENV["GOSU_TEST_INTERACTIVE"]
+    
+    STDOUT.puts message + 'Type (Y)es or (N)o and ENTER'
+    yield if block_given?
+    assert STDIN.gets.chomp =~ /[yY]/, "User answered 'No' to '#{message}'"
+  end
+  
   def interactive_gui(message)
     return false unless ENV["GOSU_TEST_INTERACTIVE"]
     
