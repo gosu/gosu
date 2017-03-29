8777c149322b7d52957e9ffc63085c2e722fcd13
Holger Arndt <github@kjarrigan.de>
2017-03-29 23:17:55 +0200

    Test Suite for Gosu Part 1 (#360)
    * Add Color tests
    * Add more Gosu module tests

---
+++ test/test_color.rb
@@ -0,0 +1,59 @@
+# Encoding: UTF-8
+require_relative 'test_helper'
+
+class TestConstants < Minitest::Test
+  def test_predefined_colors
+    [:NONE, :BLACK, :GRAY, :WHITE, :AQUA, :RED, :GREEN, :BLUE, :YELLOW, :FUCHSIA, :CYAN].each do |base_color|
+      assert Gosu::Color.const_defined?(base_color)
+    end
+  end
+
+  def test_color_creation
+    # ARGB
+    assert_equal Gosu::Color::RED,   Gosu::Color.new( 0xff, 0xff, 0x00, 0x00)
+    assert_equal Gosu::Color::GREEN, Gosu::Color.argb(0xff, 0x00, 0xff, 0x00)
+    assert_equal Gosu::Color::BLUE,  Gosu::Color.rgba(0x00, 0x00, 0xff, 0xff)
+
+    # (A)HSV
+    assert_equal Gosu::Color::FUCHSIA, Gosu::Color.from_hsv(     300, 1.0, 1.0)
+    assert_equal Gosu::Color::YELLOW,  Gosu::Color.from_ahsv(255, 60, 1.0, 1.0)
+  end
+
+  def test_color_atttributes_and_types
+    color = Gosu::Color.new(255,255,255,255)
+    [:alpha, :red, :green, :blue].each do |attr|
+      assert_respond_to color, attr
+      assert_kind_of Integer, color.send(attr), "Color##{attr} should be kind of Integer"
+    end
+
+    [:hue, :saturation, :value].each do |attr|
+      assert_respond_to color, attr
+      assert_kind_of Float, color.send(attr), "Color##{attr} should be kind of Float"
+    end
+  end
+
+  def test_color_attribute_ranges
+    # alpha, red, green, blue are clamped to 0..255
+    assert_equal Gosu::Color::WHITE, Gosu::Color.new(300, 300, 300, 300)
+    assert_equal Gosu::Color::NONE,  Gosu::Color.new(-50, -50, -50, -50)
+
+    # hue wraps(!) at 360 so 361 is the same as 1 and not the maximum value 360
+    assert_equal Gosu::Color.from_ahsv(100, 1, 1.0, 1.0), Gosu::Color.from_ahsv(100, 361, 1.0, 1.0)
+
+    # saturation and value are clamped to 0.0..1.0 (since #01120e92f7a3)
+    assert_equal Gosu::Color.from_ahsv(100, 1, 1.0, 1.0), Gosu::Color.from_ahsv(100, 361,  2.0,  2.0)
+    assert_equal Gosu::Color.from_ahsv(100, 1, 0.0, 0.0), Gosu::Color.from_ahsv(100, 361, -1.0, -1.0)
+  end
+
+  def test_dup_and_gl
+    refute_equal Gosu::Color::BLACK.object_id, Gosu::Color::BLACK.dup.object_id
+    assert_equal 0xff00ff00, Gosu::Color::GREEN.gl
+  end
+
+  # introduced by #316, requested by #333 (@shawn42)
+  # this is not documented in rdoc/gosu.rb
+  def test_alias
+    assert Gosu::Color::AQUA.eql?(Gosu::Color::CYAN)
+    assert_equal Gosu::Color::AQUA.hash, Gosu::Color::AQUA.gl
+  end
+end
---
+++ test/test_gosu_module.rb
@@ -0,0 +1,112 @@
+# Encoding: UTF-8
+require_relative 'test_helper'
+
+class TestGosuModule < Minitest::Test
+  def test_drawing_primitives
+    {
+      # args: [x, y, width, height, c, z, mode]
+      draw_rect: [0, 0, 10, 10, 0xff_ffffff, 0, :default],
+
+      # args: [x, y, c]{2,4}, z, mode
+      draw_line: [0, 0, 0xff_ffffff, 10, 10, 0xff_ffffff, 0, :default],
+      draw_triangle: [0, 0, 0xff_ffffff, 10, 10, 0xff_ffffff, 0, 10, 0xff_ffffff, 0, :default],
+      draw_quad: [0, 0, 0xff_ffffff, 10, 10, 0xff_ffffff, 0, 10, 0xff_ffffff, 0, 10, 0xff_ffffff0, 0, :default],
+    }.each do |method, args|
+      assert_respond_to Gosu, method
+
+      img = Gosu.record(20, 20) do
+        Gosu.send(method, *args)
+      end
+      assert_equal Gosu::Image, img.class
+
+      # RuntimeError: Gosu::Macro cannot be rendered as Gosu::Bitmap yet
+      # that would be awesome. Making "screenshots" in general would greatly
+      # extend the possibilities of automated testing.
+      # Already requested in https://github.com/gosu/gosu/issues/317
+      # img.save("/tmp/test.png")
+    end
+  end
+
+
+  # TODO: Manipulating the current drawing context
+  # clip_to  flush  gl  record  rotate  scale  transform  translate
+
+  def test_misc
+    assert_match(/^[a-z]{2}/, Gosu.language)
+
+    assert_equal 0, Gosu.fps, 'Gosu.fps should be 0 as there is nothing drawn'
+
+    first_call = Gosu.milliseconds
+    assert_kind_of Integer, Gosu.milliseconds, 'Gosu.milliseconds should return an integer'
+    sleep 0.2
+    assert first_call < Gosu.milliseconds, 'Gosu.milliseconds should increase over time'
+
+    assert_match /(sans|Arial)/, Gosu.default_font_name
+
+    assert Gosu.screen_height >= Gosu.available_height
+    assert Gosu.screen_width >= Gosu.available_width
+
+    assert_match(/[Aa]/, Gosu.button_id_to_char(Gosu::KB_A))
+    assert_equal Gosu::KB_A, Gosu.char_to_button_id("A")
+
+    refute Gosu.button_down?(Gosu::KB_A)
+  end
+
+  def test_math_angle
+    {
+      [ 0, 0] =>   0.0,
+      [ 1, 0] =>  90.0,
+      [ 1, 1] => 135.0,
+      [ 0, 1] => 180.0,
+      [-1, 1] => 225.0,
+      [-1, 0] => 270.0,
+      [-1,-1] => 315.0,
+      [ 0,-1] =>   0.0, # 360.0
+    }.each do |point, angle|
+      assert_equal angle, Gosu.angle(0, 0, *point)
+    end
+  end
+
+  def test_math_angle_diff
+    {
+      [90, 95] =>    5.0,
+      [90, 85] =>   -5.0,
+      [90,269] =>  179.0,
+      [90,271] => -179.0,
+    }.each do |(angle1, angle2), delta|
+      assert_equal delta, Gosu.angle_diff(angle1, angle2)
+    end
+  end
+
+  def test_math_offset
+    {
+      [ 36.86, 5] => [ 3, -4], # a² + b² = c² | 3² + 4² = 5²
+      [  0.0 , 1] => [ 0, -1],
+      [ 90.0 , 1] => [ 1,  0],
+      [180.0 , 1] => [ 0,  1],
+      [-90.0 , 1] => [-1,  0],
+    }.each do |(angle,length),(dx, dy)|
+      assert_in_delta dx, Gosu.offset_x(angle, length), 0.1, "Angle: #{angle}, Length: #{length} | offset_x should be #{dx} but is #{Gosu.offset_x(angle, length)}"
+      assert_in_delta dy, Gosu.offset_y(angle, length), 0.1, "Angle: #{angle}, Length: #{length} | offset_y should be #{dy} but is #{Gosu.offset_x(angle, length)}"
+    end
+  end
+
+  # TODO: Add more test-vectors
+  def test_math_distance
+    {
+      [0,0 , 3,4] => 5, # a² + b² = c² | 3² + 4² = 5²
+      [-2,-3 , -4, 4] => 7.28,
+    }.each do |(x1,y1,x2,y2), dist|
+      assert_in_delta dist, Gosu.distance(x1, y1, x2, y2), 0.1
+    end
+  end
+
+  def test_math_random
+    100.times do
+      val = Gosu.random(5, 10)
+      assert val.is_a?(Float)
+      assert val >= 5
+      assert val < 10
+    end
+  end
+end
