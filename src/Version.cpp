e946c22aaab8f391ce4856c94064cc72f7e43806
Julian Raschke <julian@raschke.de>
2017-04-23 18:16:08 +0200

    Add mpg123 license

--- src/Version.cpp
+++ src/Version.cpp
@@ -9,14 +9,16 @@ const std::string Gosu::VERSION = std::to_string(GOSU_MAJOR_VERSION) + '.' +
 const std::string Gosu::LICENSES =
     "This software may utilize code from the following third-party libraries:\n"
     "\n"
-    "Gosu, https://www.libgosu.org, MIT License, http://opensource.org/licenses/MIT\n"
-    "SDL 2, http://www.libsdl.org, MIT License, http://opensource.org/licenses/MIT\n"
+    "Gosu, https://www.libgosu.org, MIT License, https://opensource.org/licenses/MIT\n"
+    "SDL 2, https://www.libsdl.org, MIT License, https://opensource.org/licenses/MIT\n"
 #if defined(GOSU_IS_WIN) || defined(GOSU_IS_X)
     "libsndfile, http://www.mega-nerd.com/libsndfile, GNU LGPL 3, "
-        "http://www.gnu.org/copyleft/lesser.html\n"
+        "https://www.gnu.org/copyleft/lesser.html\n"
+    "mpg123, https://mpg123.de, GNU LGPL 3, "
+        "https://www.gnu.org/copyleft/lesser.html\n"
 #endif
 #if defined(GOSU_IS_WIN)
     "OpenAL Soft, http://kcat.strangesoft.net/openal.html, GNU LGPL 2, "
-        "http://www.gnu.org/licenses/old-licenses/lgpl-2.0.html\n"
+        "https://www.gnu.org/licenses/old-licenses/lgpl-2.0.html\n"
 #endif
     ;
