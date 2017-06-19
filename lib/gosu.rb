cc7bbe19497937c9ca80ff9f2c8a2c9c82ff925f
Julian Raschke <julian@raschke.de>
2017-06-19 22:34:06 +0200

    Use RubyInstaller.add_dll_directory, closes #407

--- lib/gosu.rb
+++ lib/gosu.rb
@@ -1,14 +1,22 @@
-require "rbconfig"
+require 'rbconfig'
 
 if RUBY_PLATFORM =~ /mswin$|mingw32|mingw64|win32\-|\-win32/
   binary_path = File.dirname(__FILE__)
   # 64-bit builds of Windows use "x64-mingw32" as RUBY_PLATFORM
   binary_path += "64" if RUBY_PLATFORM =~ /^x64-/
   
-  # Add this gem to the PATH on Windows so that bundled DLLs can be found.
-  # When running through Ocra on Windows, we need to be careful to preserve the ENV["PATH"]
-  # encoding (see #385).
-  ENV["PATH"] = "#{binary_path.encode ENV["PATH"].encoding};#{ENV["PATH"]}"
+  begin
+    # Make DLLs available as shown here:
+	# https://github.com/oneclick/rubyinstaller2/wiki/For-gem-developers
+    require 'ruby_installer'
+    RubyInstaller::Runtime.add_dll_directory(binary_path)
+  rescue LoadError
+    # Add this gem to the PATH on Windows so that bundled DLLs can be found.
+    # When running through Ocra on Windows, we need to be careful to preserve the ENV["PATH"]
+    # encoding (see #385).
+	path_encoding = ENV["PATH"].encoding
+    ENV["PATH"] = "#{binary_path.encode(path_encoding)};#{ENV["PATH"]}"
+  end
   
   # Add the correct lib directory for the current version of Ruby (major.minor).
   $LOAD_PATH.unshift File.join(binary_path, RUBY_VERSION[/^\d+.\d+/])
