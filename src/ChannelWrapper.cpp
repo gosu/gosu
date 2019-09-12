1f2b42f0a2f4436dfc701890e6c37d349f1e2925
Cyberarm <matthewlikesrobots@gmail.com>
2019-09-12 17:33:39 -0500

    Implement Gosu::Channel and Gosu::Sample

---
+++ src/ChannelWrapper.cpp
@@ -0,0 +1,40 @@
+#include <Gosu/Audio.hpp>
+#include <Gosu/Channel.h>
+
+extern "C" {
+  Gosu_Channel* Gosu_Channel_create(Gosu_Channel* channel)
+  {
+    return channel;
+  }
+
+  bool Gosu_Channel_playing(Gosu_Channel* channel) {
+    return reinterpret_cast<Gosu::Channel*>( channel )->playing();
+  }
+  void Gosu_Channel_pause(Gosu_Channel* channel) {
+    reinterpret_cast<Gosu::Channel*>( channel )->pause();
+  }
+  bool Gosu_Channel_paused(Gosu_Channel* channel) {
+    return reinterpret_cast<Gosu::Channel*>( channel )->paused();
+  }
+  void Gosu_Channel_resume(Gosu_Channel* channel) {
+    reinterpret_cast<Gosu::Channel*>( channel )->resume();
+  }
+  void Gosu_Channel_stop(Gosu_Channel* channel) {
+    reinterpret_cast<Gosu::Channel*>( channel )->stop();
+  }
+
+  void Gosu_Channel_set_volume(Gosu_Channel* channel, double volume) {
+    reinterpret_cast<Gosu::Channel*>( channel )->set_volume(volume);
+  }
+  void Gosu_Channel_set_speed(Gosu_Channel* channel, double speed) {
+    reinterpret_cast<Gosu::Channel *>(channel)->set_speed(speed);
+  }
+  void Gosu_Channel_set_pan(Gosu_Channel* channel, double pan) {
+    reinterpret_cast<Gosu::Channel *>(channel)->set_pan(pan);
+  }
+
+  void Gosu_Channel_destroy(Gosu_Channel* channel)
+  {
+    delete( reinterpret_cast<Gosu::Channel*>( channel) );
+  }
+}
---
+++ src/SampleWrapper.cpp
@@ -0,0 +1,30 @@
+#include <Gosu/Audio.hpp>
+#include <Gosu/Sample.h>
+
+extern "C" {
+
+Gosu_Sample *Gosu_Sample_create(const char *filename)
+{
+  return reinterpret_cast<Gosu_Sample *>( new Gosu::Sample(filename) );
+}
+
+void Gosu_Sample_destroy(Gosu_Sample *sample)
+{
+  delete( reinterpret_cast<Gosu_Sample *>( sample ) );
+}
+
+Gosu_Channel *Gosu_Sample_play(Gosu_Sample *sample, double volume, double speed, bool looping)
+{
+  Gosu::Channel channel = reinterpret_cast<Gosu::Sample *>( sample )->play(volume, speed, looping);
+
+  return reinterpret_cast<Gosu_Channel *>( new Gosu::Channel(channel) );
+}
+
+Gosu_Channel *Gosu_Sample_play_pan(Gosu_Sample *sample, double pan, double volume, double speed, bool looping)
+{
+  Gosu::Channel channel = reinterpret_cast<Gosu::Sample *>( sample )->play_pan(pan, volume, speed, looping);
+
+  return reinterpret_cast<Gosu_Channel *>( new Gosu::Channel(channel) );
+}
+
+}
