0b5f557abff54cccacb2d3db73757c19d6ae422c
Cyberarm <matthewlikesrobots@gmail.com>
2020-10-04 17:58:37 -0500

    'update' _VSC_VER requirement to 1927 from 1900 for the rubies config.h

--- dependencies/msvcrt-ruby/ruby-2.6.0/x64-mswin64_140/ruby/config.h
+++ dependencies/msvcrt-ruby/ruby-2.6.0/x64-mswin64_140/ruby/config.h
@@ -1,7 +1,7 @@
 #ifndef INCLUDE_RUBY_CONFIG_H
 #define INCLUDE_RUBY_CONFIG_H 1
-#if _MSC_VER != 1900
-#error MSC version unmatch: _MSC_VER: 1900 is expected.
+#if _MSC_VER != 1927
+#error MSC version unmatch: _MSC_VER: 1927 is expected.
 #endif
 #define RUBY_MSVCRT_VERSION 140
 #define STDC_HEADERS 1
