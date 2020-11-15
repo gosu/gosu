32edde6bbcf4523694fe22fd5d121ed06c2fedc9
Julian Raschke <julian@raschke.de>
2020-11-15 13:42:31 +0100

    Minimal local reproducer

--- test/test_window.rb
+++ test/test_window.rb
@@ -9,69 +9,13 @@ require_relative "test_helper"
 # - Calling close() between calls to tick() should make only the next call to tick() return false.
 
 class TestWindow < Minitest::Test
-  include InteractiveTests
-  
   class ShyWindow < Gosu::Window
-    def update
-      close!
-    end
-  end
-  
-  def test_reopen
-    window = ShyWindow.new(200, 150)
-    assert !window.tick
-    window.show
-    assert !window.tick
-  end
-  
-  
-  class StubbornWindow < Gosu::Window
-    def close
-      # Do not call super
-    end
-    
-    def button_down(id)
-      close! if id == Gosu::KB_ESCAPE
+    def needs_redraw?
+      false
     end
   end
 
-  def test_close_callback
-    window = StubbornWindow.new(200, 150)
-    # window.show # <- for manual testing
-    assert window.tick
-    window.close
-    assert window.tick
-    window.close!
-    assert !window.tick
-    assert window.tick
-  end
-  
-  class DropWindow < Gosu::Window
-    def initialize
-      super(400, 400)
-      self.caption = "Drop any number of files here!" 
-      @font = Gosu::Font.new(20)
-      @files = []
-    end
-    
-    def needs_cursor?
-      true
-    end
-    
-    def drop(path)
-      @files << path
-    end
-    
-    def draw
-      @files.each_with_index do |fname, idx|
-        @font.draw(fname, 10, idx * 20, 0)
-      end
-    end
-  end
-  
-  def test_drag_and_drop
-    interactive_gui("Drop any number of files into the window. Do all filenames appear?") do
-      DropWindow.new
-    end
+  def test_reopen
+    ShyWindow.new(200, 150).tick
   end
 end
