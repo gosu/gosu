eacac137874ff9f70e6f20f0c3d2535e4840d7b5
Julian Raschke <julian@raschke.de>
2020-11-29 19:41:19 +0100

    More detailed error message in test_image_io.rb

--- test/test_image_io.rb
+++ test/test_image_io.rb
@@ -5,7 +5,7 @@ class TestImageIO < Minitest::Test
   include TestHelper
   
   # All images in each group (key) are saved and then compared to the expected image (value).
-  # The alpha group uses bmp24 for comparison because all 'empty' pixels will reliably be 00000000.
+  # The alpha group uses bmp24 for comparison because all 'empty' pixels will be turned into fuchsia.
   # The no-alpha group uses png32 for comparison because it is lossless.
   GROUPS = { "alpha" => "alpha-bmp24.bmp", "no-alpha" => "no-alpha-png32.png" }
 
@@ -19,11 +19,13 @@ class TestImageIO < Minitest::Test
             image = Gosu::Image.new(filename)
 
             dimensions ||= [image.width, image.height]
-            assert_equal dimensions, [image.width, image.height]
+            assert_equal dimensions, [image.width, image.height],
+                "#{filename} had unexpected dimensions after loading"
 
             output_filename = File.join(tmpdir, expected)
             image.save output_filename
-            assert_equal [File.binread(expected)].pack("m"), [File.binread(output_filename)].pack("m")
+            assert_equal [File.binread(expected)].pack("m"), [File.binread(output_filename)].pack("m"),
+                "#{filename} was different from reference image (#{expected}) when written to file"
           end
         end
       end
