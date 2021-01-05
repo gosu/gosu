5876520be273da21980c4657810b80b8478ee8d4
Julian Raschke <julian@raschke.de>
2021-01-05 23:42:06 +0100

    Refactor ffi/Gosu_TextInput.{h,cpp}

--- ffi/Gosu_TextInput.cpp
+++ ffi/Gosu_TextInput.cpp
@@ -1,101 +1,86 @@
+#include "Gosu_TextInput.h"
 #include <Gosu/Gosu.hpp>
 
-namespace Gosu
+struct Gosu_TextInput : public Gosu::TextInput
 {
-class TextInputForWrapper : public Gosu::TextInput
-{
-public:
-    TextInputForWrapper();
-    std::string filter(std::string text) const override;
-    std::function<void (const char *text)> filter_callback;
-    std::string filter_result = "";
+    std::string filter(std::string text) const override
+    {
+        if (filter_callback) {
+            filter_callback(text.c_str());
+            // We now expect the result to be stored using Gosu_TextInput_set_filter_result.
+            return filter_result;
+        }
+        return Gosu::TextInput::filter(text);
+    }
+
+    std::function<void(const char* text)> filter_callback;
+    std::string filter_result;
 };
-} // namespace Gosu
 
-Gosu::TextInputForWrapper::TextInputForWrapper() : Gosu::TextInput()
+GOSU_FFI_API Gosu_TextInput* Gosu_TextInput_create()
 {
+    return new Gosu_TextInput{};
 }
 
-std::string Gosu::TextInputForWrapper::filter(std::string text) const
+GOSU_FFI_API void Gosu_TextInput_destroy(Gosu_TextInput* text_input)
 {
-    if (filter_callback != nullptr) {
-        filter_callback(text.c_str());
-        return filter_result;
-    }
-    else {
-        return text;
-    }
+    delete text_input;
 }
 
-extern "C" {
-#include "Gosu_TextInput.h"
-
-Gosu_TextInput *Gosu_TextInput_create()
+GOSU_FFI_API const char* Gosu_TextInput_text(Gosu_TextInput* text_input)
 {
-    return reinterpret_cast<Gosu_TextInput *>(new Gosu::TextInputForWrapper());
+    static thread_local std::string text;
+    text = text_input->text();
+    return text.c_str();
 }
 
-const char *Gosu_TextInput_text(Gosu_TextInput *text_input)
+GOSU_FFI_API void Gosu_TextInput_set_text(Gosu_TextInput* text_input, const char* text)
 {
-    thread_local std::string string;
-    string = reinterpret_cast<Gosu::TextInputForWrapper *>(text_input)->text();
-
-    return string.c_str();
+    text_input->set_text(text);
 }
 
-void Gosu_TextInput_set_text(Gosu_TextInput *text_input, const char *text)
+GOSU_FFI_API int Gosu_TextInput_caret_pos(Gosu_TextInput* text_input)
 {
-    reinterpret_cast<Gosu::TextInputForWrapper *>(text_input)->set_text(text);
+    return text_input->caret_pos();
 }
 
-unsigned Gosu_TextInput_caret_pos(Gosu_TextInput *text_input)
+GOSU_FFI_API void Gosu_TextInput_set_caret_pos(Gosu_TextInput* text_input, int pos)
 {
-    return reinterpret_cast<Gosu::TextInputForWrapper *>(text_input)->caret_pos();
+    text_input->set_caret_pos(pos);
 }
 
-void Gosu_TextInput_set_caret_pos(Gosu_TextInput *text_input, unsigned pos)
+GOSU_FFI_API int Gosu_TextInput_selection_start(Gosu_TextInput* text_input)
 {
-    return reinterpret_cast<Gosu::TextInputForWrapper *>(text_input)->set_caret_pos(pos);
+    return text_input->selection_start();
 }
 
-unsigned Gosu_TextInput_selection_start(Gosu_TextInput *text_input)
+GOSU_FFI_API void Gosu_TextInput_set_selection_start(Gosu_TextInput* text_input, int pos)
 {
-    return reinterpret_cast<Gosu::TextInputForWrapper *>(text_input)->selection_start();
+    return text_input->set_selection_start(pos);
 }
 
-void Gosu_TextInput_set_selection_start(Gosu_TextInput *text_input, unsigned pos)
+GOSU_FFI_API void Gosu_TextInput_set_filter(Gosu_TextInput* text_input,
+                                            void function(void*, const char*), void* data)
 {
-    return reinterpret_cast<Gosu::TextInputForWrapper *>(text_input)->set_selection_start(pos);
+    text_input->filter_callback = [=](const char* text) { function(data, text); };
 }
 
-void Gosu_TextInput_set_filter(Gosu_TextInput *text_input, void function(void *data, const char *text), void *data)
+GOSU_FFI_API void Gosu_TextInput_set_filter_result(Gosu_TextInput* text_input, const char* result)
 {
-    reinterpret_cast<Gosu::TextInputForWrapper *>(text_input)->filter_callback = [=](const char *text) { function(data, text); };
+    text_input->filter_result = result;
 }
 
-void Gosu_TextInput_set_filter_result(Gosu_TextInput *text_input, const char *result)
+GOSU_FFI_API void Gosu_TextInput_insert_text(Gosu_TextInput* text_input, const char* text)
 {
-    reinterpret_cast<Gosu::TextInputForWrapper *>(text_input)->filter_result = result;
+    text_input->insert_text(text);
 }
 
-void Gosu_TextInput_insert_text(Gosu_TextInput *text_input, const char *text)
+GOSU_FFI_API void Gosu_TextInput_delete_backward(Gosu_TextInput* text_input)
 {
-    reinterpret_cast<Gosu::TextInputForWrapper *>(text_input)->insert_text(text);
+    text_input->delete_backward();
 }
 
-void Gosu_TextInput_delete_backward(Gosu_TextInput *text_input)
+GOSU_FFI_API void Gosu_TextInput_delete_forward(Gosu_TextInput* text_input)
 {
-    reinterpret_cast<Gosu::TextInputForWrapper *>(text_input)->delete_backward();
-}
-
-void Gosu_TextInput_delete_forward(Gosu_TextInput *text_input)
-{
-    reinterpret_cast<Gosu::TextInputForWrapper *>(text_input)->delete_forward();
-}
-
-void Gosu_TextInput_destroy(Gosu_TextInput *text_input)
-{
-    delete (reinterpret_cast<Gosu::TextInputForWrapper *>(text_input));
-}
-
+    text_input->delete_forward();
 }
