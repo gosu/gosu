4849ea7ed658b217dcbdd1669a71057737a05bdb
Julian Raschke <julian@raschke.de>
2021-01-09 21:35:55 +0100

    Refactor ffi/Gosu_Color.{h,cpp}

--- ffi/Gosu_Color.h
+++ ffi/Gosu_Color.h
@@ -1,40 +1,31 @@
 #pragma once
 
 #include "Gosu_FFI.h"
+#include "stdint.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+GOSU_FFI_API uint32_t Gosu_Color_create(uint32_t argb);
+GOSU_FFI_API uint32_t Gosu_Color_create_argb(uint8_t a, uint8_t r, uint8_t g, uint8_t b);
+GOSU_FFI_API uint32_t Gosu_Color_create_from_hsv(double h, double s, double v);
+GOSU_FFI_API uint32_t Gosu_Color_create_from_ahsv(uint8_t alpha, double h, double s, double v);
 
-typedef unsigned char Gosu_Color_Channel;
+GOSU_FFI_API uint8_t Gosu_Color_alpha(uint32_t color);
+GOSU_FFI_API uint8_t Gosu_Color_red(uint32_t color);
+GOSU_FFI_API uint8_t Gosu_Color_green(uint32_t color);
+GOSU_FFI_API uint8_t Gosu_Color_blue(uint32_t color);
+GOSU_FFI_API uint32_t Gosu_Color_set_alpha(uint32_t color, uint8_t value);
+GOSU_FFI_API uint32_t Gosu_Color_set_red(uint32_t color, uint8_t value);
+GOSU_FFI_API uint32_t Gosu_Color_set_green(uint32_t color, uint8_t value);
+GOSU_FFI_API uint32_t Gosu_Color_set_blue(uint32_t color, uint8_t value);
 
-GOSU_FFI_API unsigned Gosu_Color_create(unsigned argb);
-GOSU_FFI_API unsigned Gosu_Color_create_argb(Gosu_Color_Channel a, Gosu_Color_Channel r, Gosu_Color_Channel g, Gosu_Color_Channel b);
-GOSU_FFI_API unsigned Gosu_Color_create_from_hsv(double h, double s, double v);
-GOSU_FFI_API unsigned Gosu_Color_create_from_ahsv(Gosu_Color_Channel alpha, double h, double s, double v);
+GOSU_FFI_API double Gosu_Color_hue(uint32_t color);
+GOSU_FFI_API double Gosu_Color_saturation(uint32_t color);
+GOSU_FFI_API double Gosu_Color_value(uint32_t color);
+GOSU_FFI_API uint32_t Gosu_Color_set_hue(uint32_t color, double value);
+GOSU_FFI_API uint32_t Gosu_Color_set_saturation(uint32_t color, double value);
+GOSU_FFI_API uint32_t Gosu_Color_set_value(uint32_t color, double value);
 
-GOSU_FFI_API Gosu_Color_Channel Gosu_Color_alpha(unsigned color);
-GOSU_FFI_API Gosu_Color_Channel Gosu_Color_red(unsigned color);
-GOSU_FFI_API Gosu_Color_Channel Gosu_Color_green(unsigned color);
-GOSU_FFI_API Gosu_Color_Channel Gosu_Color_blue(unsigned color);
-GOSU_FFI_API unsigned Gosu_Color_set_alpha(unsigned color, Gosu_Color_Channel value);
-GOSU_FFI_API unsigned Gosu_Color_set_red(unsigned color, Gosu_Color_Channel value);
-GOSU_FFI_API unsigned Gosu_Color_set_green(unsigned color, Gosu_Color_Channel value);
-GOSU_FFI_API unsigned Gosu_Color_set_blue(unsigned color, Gosu_Color_Channel value);
+GOSU_FFI_API uint32_t Gosu_Color_argb(uint32_t color);
+GOSU_FFI_API uint32_t Gosu_Color_bgr(uint32_t color);
+GOSU_FFI_API uint32_t Gosu_Color_abgr(uint32_t color);
 
-GOSU_FFI_API double Gosu_Color_hue(unsigned color);
-GOSU_FFI_API double Gosu_Color_saturation(unsigned color);
-GOSU_FFI_API double Gosu_Color_value(unsigned color);
-GOSU_FFI_API unsigned Gosu_Color_set_hue(unsigned color, double value);
-GOSU_FFI_API unsigned Gosu_Color_set_saturation(unsigned color, double value);
-GOSU_FFI_API unsigned Gosu_Color_set_value(unsigned color, double value);
-
-GOSU_FFI_API unsigned Gosu_Color_argb(unsigned color);
-GOSU_FFI_API unsigned Gosu_Color_bgr(unsigned color);
-GOSU_FFI_API unsigned Gosu_Color_abgr(unsigned color);
-
-GOSU_FFI_API unsigned Gosu_Color_gl(unsigned color);
-
-#ifdef __cplusplus
-}
-#endif
+GOSU_FFI_API uint32_t Gosu_Color_gl(uint32_t color);
