f38664c5aaa29efd048f00a4c5847c0a4cca554d
Julian Raschke <julian@raschke.de>
2021-01-16 19:34:45 +0100

    Refactor ffi/Gosu_Song.{h,cpp}

--- ffi/Gosu_Song.cpp
+++ ffi/Gosu_Song.cpp
@@ -1,52 +1,61 @@
-#include <Gosu/Audio.hpp>
 #include "Gosu_Song.h"
+#include <Gosu/Audio.hpp>
 
-extern "C" {
-  Gosu_Song* Gosu_Song_create(const char* filename)
-  {
-    return reinterpret_cast<Gosu_Song*>( new Gosu::Song(filename) );
-  }
+struct Gosu_Song : Gosu::Song
+{
+    [[maybe_unused]] // silence buggy warning - this is being used, see below.
+    explicit Gosu_Song(const std::string& filename)
+    : Gosu::Song{filename}
+    {
+    }
+};
 
-  void Gosu_Song_destroy(Gosu_Song* song)
-  {
-    delete( reinterpret_cast<Gosu::Song*>( song ));
-  }
+GOSU_FFI_API Gosu_Song* Gosu_Song_create(const char* filename)
+{
+    return new Gosu_Song{filename};
+}
 
-  void Gosu_Song_play(Gosu_Song* song, bool looping)
-  {
-    reinterpret_cast<Gosu::Song*>( song )->play(looping);
-  }
+GOSU_FFI_API void Gosu_Song_destroy(Gosu_Song* song)
+{
+    delete song;
+}
 
-  bool Gosu_Song_playing(Gosu_Song* song)
-  {
-    return reinterpret_cast<Gosu::Song*>( song )->playing();
-  }
+GOSU_FFI_API void Gosu_Song_play(Gosu_Song* song, bool looping)
+{
+    song->play(looping);
+}
 
-  void Gosu_Song_pause(Gosu_Song* song)
-  {
-    reinterpret_cast<Gosu::Song*>( song )->pause();
-  }
+GOSU_FFI_API bool Gosu_Song_playing(Gosu_Song* song)
+{
+    return song->playing();
+}
 
-  bool Gosu_Song_paused(Gosu_Song* song)
-  {
-    return reinterpret_cast<Gosu::Song*>( song )->paused();
-  }
+GOSU_FFI_API void Gosu_Song_pause(Gosu_Song* song)
+{
+    song->pause();
+}
 
-  void Gosu_Song_stop(Gosu_Song* song)
-  {
-    reinterpret_cast<Gosu::Song*>( song )->stop();
-  }
+GOSU_FFI_API bool Gosu_Song_paused(Gosu_Song* song)
+{
+    return song->paused();
+}
 
-  double Gosu_Song_volume(Gosu_Song* song){
-    return reinterpret_cast<Gosu::Song*>( song )->volume();
-  }
+GOSU_FFI_API void Gosu_Song_stop(Gosu_Song* song)
+{
+    song->stop();
+}
 
-  void Gosu_Song_set_volume(Gosu_Song* song, double volume){
-    return reinterpret_cast<Gosu::Song*>( song )->set_volume(volume);
-  }
+GOSU_FFI_API double Gosu_Song_volume(Gosu_Song* song)
+{
+    return song->volume();
+}
 
-  Gosu_Song* Gosu_Song_current_song()
-  {
+GOSU_FFI_API void Gosu_Song_set_volume(Gosu_Song* song, double volume)
+{
+    return song->set_volume(volume);
+}
+
+GOSU_FFI_API Gosu_Song* Gosu_Song_current_song(void)
+{
     return reinterpret_cast<Gosu_Song*>(Gosu::Song::current_song());
-  }
 }
