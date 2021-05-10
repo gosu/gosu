26afa2e17d2bd80de6c6d744a87e250b2688e079
cyberarm <matthewlikesrobots@gmail.com>
2021-05-10 15:40:13 +0000

    libvlc_video_set_callbacks 'working' without segfaulting :joy:

--- src/Video.cpp
+++ src/Video.cpp
@@ -1,6 +1,5 @@
 #ifdef GOSU_ENABLE_VIDEO
 
-#include "GraphicsImpl.hpp"
 #include <Gosu/Video.hpp>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_syswm.h>
@@ -10,15 +9,18 @@ struct Gosu::Video::Impl
     struct context
     {
         bool locked = false;
+        Gosu::Image* image;
         Gosu::Bitmap* bitmap;
     };
 
     double volume_ = 1.0;
     bool looping_ = false;
     double duration_;
-    unsigned video_width_ = 0, video_height_ = 0;
+    unsigned video_width_ = 640, video_height_ = 480;
     unsigned image_width_ = 0, image_height_ = 0;
     unsigned image_flags_ = ImageFlags::IF_SMOOTH;
+
+    struct context ctx_;
 
     libvlc_instance_t *vlc_instance_;
     libvlc_media_t *vlc_media_;
@@ -26,37 +28,44 @@ struct Gosu::Video::Impl
 
     static void* lock_video(void* data, void** p_pixels)
     {
-        struct context* ctx = (context *) data;
-
+        puts("REACHED lock_video");
+        struct context* ctx = (context*) data;
+        puts("REACHED ctx");
         ctx->locked = true;
+        puts("REACHED LOCKER");
         *p_pixels = ctx->bitmap->data();
+        puts("REACHED DATA");
+
+        puts("REACHED LOCK");
         return NULL;
     }
 
-    static void unlock_video(void* data, void* id, void* const* p_pixels)
+    static void unlock_video(void* data, void* picture, void* const* p_pixels)
     {
         struct context* ctx = (context*) data;
+        ctx->locked = false;
+        puts("REACHED UNLOCK");
 
-        assert(id == NULL);
+        assert (picture == NULL);
     }
-    static void display(void* data, void* id)
-    {
 
+    static void display(void* data, void* picture)
+    {
+        puts("REACHED DISPLAY");
+        struct context* ctx = (context*) data;
     }
 
     void apply_volume()
     {
-        // Do stuff
-        // volume();
+        libvlc_audio_set_volume(vlc_media_player_, volume());
     }
 
     void initialize_video(const std::string filename)
     {
-        struct context context;
-
-        vlc_instance_ = libvlc_new(0, NULL);
+        vlc_instance_ = libvlc_new(0, nullptr);
         vlc_media_ = libvlc_media_new_path(vlc_instance_, filename.c_str());
         vlc_media_player_ = libvlc_media_player_new_from_media(vlc_media_);
+        libvlc_media_release(vlc_media_);
 
         // Start playing for a few microseconds to get metadata
         libvlc_media_player_play(vlc_media_player_);
@@ -72,9 +81,15 @@ struct Gosu::Video::Impl
 
         libvlc_media_player_stop(vlc_media_player_);
 
-        // TODO: Resolve segfault use this callback instead of making VLC draw to window
-        // libvlc_video_set_callbacks(vlc_media_player_, lock_video, nullptr, nullptr, nullptr);
-        // libvlc_video_set_format(vlc_media_player_, "RGBA", video_width_, video_height_, video_width_ * 4);
+        ctx_.bitmap = new Bitmap();
+        ctx_.bitmap->resize(width(), height(), Gosu::Color::BLUE);
+        ctx_.image = new Image(*ctx_.bitmap, image_flags_);
+
+        // TODO: Resolve segfault and use this callback instead of making VLC draw to window
+        libvlc_video_set_callbacks(vlc_media_player_, lock_video, unlock_video, display, &ctx_);
+        libvlc_video_set_format(vlc_media_player_, "RGBA", width(), height(), video_width_ * 4);
+        // VLC 4 function for OpenGL: https://github.com/videolan/vlc/blob/master/doc/libvlc/sdl_opengl_player.cpp
+        // libvlc_video_set_output_callbacks(vlc_media_player_);
     }
 
 public:
@@ -116,34 +131,33 @@ public:
 
     Gosu::Image* image()
     {
-        return new Gosu::Image(Bitmap(width(), height(), Gosu::Color::BLUE));
+        return ctx_.image;// new Gosu::Image(Bitmap(width(), height(), Gosu::Color::BLUE));
     }
 
     void play(bool looping)
     {
         looping_ = looping;
-        libvlc_media_player_set_xwindow(vlc_media_player_, SDL_GetWindowID(Gosu::shared_window()));
         libvlc_media_player_play(vlc_media_player_);
     }
 
     void pause()
     {
+        libvlc_media_player_pause(vlc_media_player_);
     }
 
     void stop()
     {
-        libvlc_media_player_set_xwindow(vlc_media_player_, 0);
         libvlc_media_player_stop(vlc_media_player_);
     }
 
     bool playing()
     {
-        return false;
+        return libvlc_media_player_is_playing(vlc_media_player_);
     }
 
     bool paused()
     {
-        return false;
+        return !playing();
     }
 
     double volume() const
